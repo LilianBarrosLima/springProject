dda6957bc57d8d916578fada8fb6dc67df53ef50
Lilian Lima <lilian.barros08@yahoo.com.br>
2022-03-15 20:51:12 -0300

    Entity User and Resource

---
+++ src/main/java/com/lilianlima/springProject/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.lilianlima.springProject.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lilianlima.springProject.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User (1l, "Harry", "Harry@Potter.com", "9999-9999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
